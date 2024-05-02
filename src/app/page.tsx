4bc1f0bf97f121f6fe508420be58ac59825a4957
Luke David Turnbull <lukedturnbull@gmail.com>
2024-05-02 22:06:50 +1000

    added connection to db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/0b8ab7c6-61c7-463c-b6d2-b16c7864b287-m48bhc.png",
@@ -13,12 +13,21 @@ const mockImages = mockUrls.map((url, index) => {
   };
 });
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            <h1>{post.name}</h1>
+          </div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
